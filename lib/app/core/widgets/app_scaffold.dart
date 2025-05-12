bbbb3d9383daf44e5ae53a97b497c057d8da4226
Marwan Sayed <marwan.sayed@mobidevlabs.com>
2025-05-12 17:06:07 +0300

    temp

--- lib/app/core/widgets/app_scaffold.dart
+++ lib/app/core/widgets/app_scaffold.dart
@@ -1,17 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class AppScaffold extends StatelessWidget {
   const AppScaffold({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: SafeArea(
-        child: Column(
-          children: [
-            Text('Hello'),
-          ],
-        ),
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(
+            child: Container(
+              width: double.infinity,
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                    Color(0xFF3A4FA5), // #3A4FA5
+                    Color(0xFF161E3F), // #161E3F
+                  ],
+                ),
+              ),
+              child: const Text('Hello'),
+            ),
+          ),
+          Container(
+            height: 800.h,
+            child: const Text('Hello'),
+          ),
+        ],
       ),
     );
   }
