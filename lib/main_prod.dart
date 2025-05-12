16f6e0ea921e51cee59f68c102f58915ed8c7ff5
Marwan Sayed <marwan.sayed@mobidevlabs.com>
2025-05-12 17:05:52 +0300

    Main Common File

--- lib/main_prod.dart
+++ lib/main_prod.dart
@@ -1,42 +1,6 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:get/get.dart';
-import 'app/routes/app_pages.dart';
+import 'main_common.dart';
 
-class MyApp extends StatelessWidget {
-  final String environment;
-
-  const MyApp({
-    Key? key,
-    required this.environment,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return GetMaterialApp(
-      title: "Application ${environment.toUpperCase()}",
-      initialRoute: AppPages.INITIAL,
-      getPages: AppPages.routes,
-    );
-  }
-}
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  ///: TODO: Add firebase plugins
-  /// and don`t forget to add google-services.json file to android/app/
-  /* if (!UniversalPlatform.isWeb) {
-    if (!kReleaseMode) {
-      FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(false);
-    } else {
-      FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(true);
-    }
-    FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
-  } */
-  await ScreenUtil.ensureScreenSize();
+Future<void> main() async {
   const environment = 'prod';
-  runApp(const MyApp(
-    environment: environment,
-  ));
+  await mainCommon(environment);
 }
